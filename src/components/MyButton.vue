be0000ef247828540233094d3110cac70ff83a1a
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-10-24 18:13:47 +0900

    vue-property-decoratorの使い方: @Emitについて

---
+++ src/components/MyButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="onClick">MyButton</button>
+</template>
+
+<script lang="ts">
+import {
+  Vue, Component, Prop, Emit,
+} from 'vue-property-decorator';
+
+@Component
+export default class MyButton extends Vue {
+  private count: number = 0;
+
+  @Prop() public greet?: string;
+
+  /* eslint class-methods-use-this: "off" */
+  @Emit()
+  public clicked(count: number) {}
+
+  public onClick() {
+    /* eslint no-alert: "off" */
+    alert(this.greet);
+    this.count += 1;
+    this.clicked(this.count);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
