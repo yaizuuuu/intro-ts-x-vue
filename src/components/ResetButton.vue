dd8ddba2973e154095e4df851c53066af9edafae
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-10-25 18:16:18 +0900

    vue-property-decoratorの使い方: ライフサイクルフックの書き方, プロパティの書き方

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+import {
+  Vue, Component, Prop, Emit,
+} from 'vue-property-decorator';
+
+@Component
+export default class ResetButton extends Vue {
+  public initialValue!: string;
+
+  @Prop() public value!: string;
+
+  /* eslint class-methods-use-this: "off" */
+  @Emit()
+  input(value: string) {}
+
+  public onClick() {
+    this.input(this.initialValue);
+  }
+
+  public created() {
+    this.initialValue = this.value;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
