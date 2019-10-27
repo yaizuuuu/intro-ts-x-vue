c827a2b31acec8c1615c148ecf317ce20220306f
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-10-27 20:59:06 +0900

    (デコレータなし)componentに型付け: typeでdataを型付け

---
+++ src/components/TsPracticeVue1.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export type Todo = {
+  id: string
+  task: string
+  done: boolean
+};
+
+export type Data = {
+  firstName: string
+  lastName: string
+  todos: Todo[]
+};
+
+export default Vue.extend({
+  name: 'TsPracticeVue1',
+  data: (): Data => ({
+    firstName: 'Taro',
+    lastName: 'Yamada',
+    todos: [],
+  }),
+  props: {
+    msg: String,
+  },
+  computed: {
+    message(): string {
+      return this.msg;
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
