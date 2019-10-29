2397aeb4c4932ddba0e0db04201b4db48e71ca69
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-10-29 21:42:55 +0900

    (デコレータなし)componentに型付け: typeでpropを型付け

---
+++ src/components/ts_practice/TsPracticeVue2_1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todo">
+    {{ myTask }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export type Todo = {
+  id: string
+  task: string
+  done: boolean
+};
+
+export default Vue.extend({
+  props: {
+    todo: Object as PropType<Todo>,
+  },
+  computed: {
+    myTask(): string {
+      return this.todo.task;
+    },
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ts_practice/TsPracticeVue2_2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todos">
+    <div v-for="todo in todos" :key="todo.id">
+      <TsPracticeVue21 :todo="todo"></TsPracticeVue21>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TsPracticeVue21, { Todo } from '@/components/ts_practice/TsPracticeVue2_1.vue';
+
+export type Data = {
+  todos: Todo[]
+};
+
+export default Vue.extend({
+  data: (): Data => ({
+    todos: [],
+  }),
+
+  components: {
+    TsPracticeVue21,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
