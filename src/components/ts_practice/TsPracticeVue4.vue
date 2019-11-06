d26930e71dfadbf3e470d0ef6ff40465ae3db581
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2019-11-06 22:25:38 +0900

    (デコレータなし)componentに型付け: methodsに型付け

--- src/components/ts_practice/TsPracticeVue4.vue
+++ src/components/ts_practice/TsPracticeVue4.vue
@@ -1,77 +1,22 @@
 <template>
+  <div @click="onClickElement"></div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export type Rect = {
-  width: number
-  height: number
-}
-
-export type Position = {
-  top: number
-  left: number
-}
-
 export default Vue.extend({
   data: () => ({
-    defaultDateLabel: '1980-01-01',
-    dataLabel: null as string | null,
-
     bounds: null as ClientRect | DOMRect | null,
   }),
-  computed: {
-    greet(): string {
-      return 'Hello World';
-    },
-    dataFromDateLabel(): Date {
-      let label = this.defaultDateLabel;
-
-      if (this.dataLabel !== null) {
-        label = this.dataLabel;
-      }
-
-      return new Date(label);
-    },
-
-    boundsInfo(): string {
-      if (this.bounds === null) {
-        return '';
-      }
-
-      return JSON.stringify(this.bounds);
-    },
-    rect(): Rect {
-      if (this.bounds === null) {
-        return {
-          width: 0,
-          height: 0,
-        };
-      }
-
-      return {
-        width: this.bounds.width,
-        height: this.bounds.height,
-      };
-    },
-    position(): Position {
-      if (this.bounds === null) {
-        return {
-          top: 0,
-          left: 0,
-        };
-      }
-
-      return {
-        top: this.bounds.top,
-        left: this.bounds.left,
-      };
-    },
-  },
   methods: {
-    onClickElement({ target }: { target: HTMLElement }): void {
-      this.bounds = target.getBoundingClientRect();
+    onClickElement(event: Event): void {
+      if (!event.isTrusted) {
+        return;
+      }
+      if (event.target instanceof HTMLElement) {
+        this.bounds = event.target.getBoundingClientRect();
+      }
     },
   },
 });
